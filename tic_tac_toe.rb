7cb9a34f5bd496fc0b71c0b880a0b05efdd4e99d
Graham <git@grahamirvine.com>
2022-05-09 16:18:37 -0600

    Created playerX and playerO subclasses

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,5 +16,42 @@ class Board
   end
 end
 
+class Player
+  @@who_turn = 1
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def player_turn
+    puts "It is #{self.name}'s turn..."
+    if @@who_turn == 1
+      puts "Where would you like to place your \"X\"?...>>"
+      x_placement = gets.chomp
+      @@who_turn = 2
+    else
+      puts "Where would you like to place your \"O\"?...>>"
+      o_placement = gets.chomp
+      @@who_turn = 1
+    end
+
+    
+  end
+end
+
+class PlayerX < Player
+end
+
+class PlayerO < Player
+end
+
 game1 = Board.new
 game1.inital_board
+
+print "What is player 1's name? >>"
+player1 = PlayerX.new(gets.chomp.to_s)
+print "\nWhat is player 2's name? >>"
+player2 = PlayerO.new(gets.chomp.to_s)
+
+player1.player_turn
