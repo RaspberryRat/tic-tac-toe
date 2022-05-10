c8c95b108c80dd2989549f1bf4480329e6528942
Graham <git@grahamirvine.com>
2022-05-10 16:07:08 -0600

    2 instances of player class created and board instance created and drawn to terminal

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,32 @@
-
-
+#require "pry-byebug"
 class Game
   def initialize
     @player1 = Player.new
     @player2 = Player.new
     @board = Board.new
   end
-
 end
 
-class Player
-  
+class Player < Game
+  attr_reader :name
+  def initialize
+    puts "What is your name?>>"
+    @name = gets.chomp
+    puts "Welcome #{self.name}"
+  end
 end
 
 class Board
+  def initialize
+    draw_board
+  end
+  def draw_board
+    3.times do
+      puts "__|__|__\n"
+    end
+  end
 
 end
+
+game1 = Game.new
+
