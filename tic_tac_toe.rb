32c83a62ed282854f5bd37fcd9c711b67c215793
Graham <git@grahamirvine.com>
2022-05-11 11:05:09 -0600

    Updated player class, created initial grid method create inital game state"

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,12 +5,17 @@ class Game
     @player2 = Player.new
     @board = Board.new
   end
+
+  
 end
 
 class Player < Game
   attr_reader :name
+  @@player_count = 0
   def initialize
-    puts "What is your name?>>"
+    @@player_count += 1
+    @@player_count == 1 ? player_name_count = "Player One" : player_name_count = "Player Two"
+    puts "#{player_name_count}, what is your name?>>"
     @name = gets.chomp
     puts "Welcome #{self.name}"
   end
@@ -18,14 +23,19 @@ end
 
 class Board
   def initialize
+    initial_grid
     draw_board
   end
+
   def draw_board
     3.times do
       puts "__|__|__\n"
     end
   end
 
+  def initial_grid
+    game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+  end
 end
 
 game1 = Game.new
